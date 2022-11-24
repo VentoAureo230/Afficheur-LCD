9705be7bcc8f3e84c0af44869c9b56d0003312dd
VentoAureo230 <anthony.meny35@gmail.com>
2022-11-24 15:46:08 +0100

    Test + code fonctionnel

--- AfficheurLCD.Test/AfficheurNombresTest.cs
+++ AfficheurLCD.Test/AfficheurNombresTest.cs
@@ -12,11 +12,103 @@ namespace AfficheurLCD.Test
             var représentation = AfficheurNombres.Représenter(chiffre);
 
             // ALORS on obtient deux barres verticales à droite
-            var attendu = "   " + Environment.NewLine +
-                          "  |" + Environment.NewLine +
-                          "  |";
+            var attendu = "   " + Environment.NewLine + "  |" + Environment.NewLine + "  |";
 
             Assert.Equal(attendu, représentation);
+        }
+        [Fact]
+        public void Test2()
+        {
+            const uint chiffre = 2;
+
+            var repr = AfficheurNombres.Représenter(chiffre);
+
+            var attendu = "  _" + Environment.NewLine + " _|" + Environment.NewLine + " |_";
+
+            Assert.Equal(attendu, repr);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            const uint chiffre = 3;
+
+            var repr = AfficheurNombres.Représenter(chiffre);
+
+            var attendu = "  _" + Environment.NewLine + " _|" + Environment.NewLine + " _|";
+
+            Assert.Equal(attendu, repr);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            const uint chiffre = 4;
+
+            var repr = AfficheurNombres.Représenter(chiffre);
+
+            var attendu = "   " + Environment.NewLine + "|_|" + Environment.NewLine + "  |";
+
+            Assert.Equal(attendu, repr);
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            const uint chiffre = 5;
+
+            var repr = AfficheurNombres.Représenter(chiffre);
+
+            var attendu = "  _" + Environment.NewLine + " |_" + Environment.NewLine + " _|";
+
+            Assert.Equal(attendu, repr);
+        }
+        [Fact]
+        public void Test6()
+        {
+            const uint chiffre = 6;
+
+            var repr = AfficheurNombres.Représenter(chiffre);
+
+            var attendu = "  _" + Environment.NewLine + " |_" + Environment.NewLine + "|_|";
+
+            Assert.Equal(attendu, repr);
+        }
+
+        [Fact]
+        public void Test7()
+        {
+            const uint chiffre = 7;
+
+            var repr = AfficheurNombres.Représenter(chiffre);
+
+            var attendu = "  _" + Environment.NewLine + "  |" + Environment.NewLine + "  |";
+            
+            Assert.Equal(attendu, repr);
+        }
+
+        [Fact]
+        public void Test8()
+        {
+            const uint chiffre = 8;
+
+            var repr = AfficheurNombres.Représenter(chiffre);
+
+            var attendu = "  _" + Environment.NewLine + "|_|" + Environment.NewLine + "|_|";
+
+            Assert.Equal(attendu, repr);
+        }
+
+        [Fact]
+        public void Test9()
+        {
+            const uint chiffre = 9;
+
+            var repr = AfficheurNombres.Représenter(chiffre);
+
+            var attendu = "  _" + Environment.NewLine + "|_|" + Environment.NewLine + " _|";
+
+            Assert.Equal(attendu, repr);
         }
     }
 }
--- AfficheurLCD/AfficheurNombres.cs
+++ AfficheurLCD/AfficheurNombres.cs
@@ -2,9 +2,37 @@
 {
     public class AfficheurNombres
     {
-        public static string Représenter(uint nombre)
+        public static string Représenter(uint chiffre)
         {
-            throw new NotImplementedException();
+            switch (chiffre)
+            {
+                case 1:
+                    return ("   " + Environment.NewLine + "  |" + Environment.NewLine + "  |");
+                case 2:
+                    return ("  _" + Environment.NewLine + " _|" + Environment.NewLine + " |_");
+                case 3:
+                    return ("  _" + Environment.NewLine + " _|" + Environment.NewLine + " _|");
+                    
+                case 4:
+                    return ("   " + Environment.NewLine + "|_|" + Environment.NewLine + "  |");
+                    
+                case 5:
+                    return ("  _" + Environment.NewLine + " |_" + Environment.NewLine + " _|");
+                    
+                case 6:
+                    return ("  _" + Environment.NewLine + " |_" + Environment.NewLine + "|_|");
+                    
+                case 7:
+                    return ("  _" + Environment.NewLine + "  |" + Environment.NewLine + "  |");
+                    
+                case 8:
+                    return ("  _" + Environment.NewLine + "|_|" + Environment.NewLine + "|_|");
+                    
+                case 9:
+                    return ("  _" + Environment.NewLine + "|_|" + Environment.NewLine + " _|");
+                default:
+                    return ("  _" + Environment.NewLine + "| |" + Environment.NewLine + "|_|");
+            }
         }
     }
 }
